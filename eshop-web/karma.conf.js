a21afd08e48a709429b4ad682ebfb84fc31aad11
sekhwelad <25sekhwelad@gmail.com>
2023-06-05 13:28:05 +0200

    New Init

--- eshop-web/karma.conf.js
+++ eshop-web/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Online-Shop'),
+      dir: require('path').join(__dirname, './coverage/eshop-web'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- eshop-web/src/app/app.component.spec.ts
+++ eshop-web/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Online-Shop'`, () => {
+  it(`should have as title 'eshop-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Online-Shop');
+    expect(app.title).toEqual('eshop-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Online-Shop app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('eshop-web app is running!');
   });
 });
